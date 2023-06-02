c2bdd06aff0a13b9bb1ce2c7d9b07f8495c7ca90
Joseph Morales <josephzx1@hotmail.com>
2023-06-02 13:45:13 -0600

    add backend

---
+++ backend/models/ticket-list.js
@@ -0,0 +1,33 @@
+const { Ticket } = require("./ticket");
+class TicketList {
+  constructor() {
+    this.ultimoNumero = 0;
+    this.pendientes = [];
+    this.asignados = [];
+  }
+  get siguienteNumero() {
+    this.ultimoNumero++;
+    return this.ultimoNumero;
+  }
+  //3 que se veran en las tarjetas y 10 en el historial
+  get utlimos13() {
+    return this.asignados.slice(0, 13);
+  }
+  crearTicket() {
+    const nuevoTicket = new Ticket(this.siguienteNumero);
+    this.pendientes.push(nuevoTicket);
+    return nuevoTicket;
+  }
+  asignarTicket() {
+    if (this.pendientes.length === 0) {
+      return null;
+    }
+    const siguienteTicket = this.pendientes.shift();
+
+    siguienteTicket.agente = agente;
+    siguienteTicket.escritorio = escritorio;
+
+    this.asignados.unshift(siguienteTicket);
+  }
+}
+module.exports = { TicketList };
